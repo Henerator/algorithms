dd5f1e03ae86b8a360814bd5585dcc33b06e68c3
Roman Makarov <roman.makarov@verifone.com>
2022-05-03 21:02:35 +0400

    min binary heap

--- src/binary-heap/binary-heap.js
+++ src/binary-heap/binary-heap.js
@@ -1,7 +1,8 @@
-class MaxBinaryHeap {
-  constructor(extractValue) {
+class BaseBinaryHeap {
+  constructor(extractValue, compareValues) {
     this.items = [];
     this.extractValue = extractValue || this.defaultExtractor;
+    this.compareValues = compareValues;
   }
 
   get size() {
@@ -32,29 +33,14 @@ class MaxBinaryHeap {
     let index = this.size - 1;
     let parentIndex = this.getParentIndex(index);
 
-    while (index > 0 && this.getValue(parentIndex) < this.getValue(index)) {
+    while (
+      index > 0 &&
+      this.compareValues(this.getValue(parentIndex), this.getValue(index)) < 0
+    ) {
       this.swap(index, parentIndex);
       index = parentIndex;
       parentIndex = this.getParentIndex(index);
     }
-  }
-
-  extractMax() {
-    const size = this.size;
-
-    if (size === 0) {
-      return null;
-    }
-
-    if (size === 1) {
-      return this.items.pop();
-    }
-
-    const max = this.items[0];
-    this.items[0] = this.items.pop();
-    this.heapify(0);
-
-    return max;
   }
 
   heapify(index) {
@@ -70,14 +56,20 @@ class MaxBinaryHeap {
 
       if (
         leftChildIndex < size &&
-        this.getValue(leftChildIndex) > this.getValue(largestIndex)
+        this.compareValues(
+          this.getValue(leftChildIndex),
+          this.getValue(largestIndex)
+        ) > 0
       ) {
         largestIndex = leftChildIndex;
       }
 
       if (
         rightChildIndex < size &&
-        this.getValue(rightChildIndex) > this.getValue(largestIndex)
+        this.compareValues(
+          this.getValue(rightChildIndex),
+          this.getValue(largestIndex)
+        ) > 0
       ) {
         largestIndex = rightChildIndex;
       }
@@ -90,6 +82,58 @@ class MaxBinaryHeap {
       index = largestIndex;
     }
   }
+
+  build(arr) {
+    this.items = arr.slice();
+
+    const middle = Math.floor(arr.length / 2) - 1;
+    for (let index = middle; index >= 0; index--) {
+      this.heapify(index);
+    }
+  }
+
+  extractRoot() {
+    const size = this.size;
+
+    if (size === 0) {
+      return null;
+    }
+
+    if (size === 1) {
+      return this.items.pop();
+    }
+
+    const root = this.items[0];
+    this.items[0] = this.items.pop();
+    this.heapify(0);
+
+    return root;
+  }
+}
+
+class MaxBinaryHeap extends BaseBinaryHeap {
+  constructor(extractValue) {
+    super(extractValue, (itemA, itemB) => {
+      return itemA - itemB;
+    });
+  }
+
+  extractMax() {
+    return this.extractRoot();
+  }
+}
+
+class MinBinaryHeap extends BaseBinaryHeap {
+  constructor(extractValue) {
+    super(extractValue, (itemA, itemB) => {
+      return itemB - itemA;
+    });
+  }
+
+  extractMin() {
+    return this.extractRoot();
+  }
 }
 
 exports.MaxBinaryHeap = MaxBinaryHeap;
+exports.MinBinaryHeap = MinBinaryHeap;
--- src/binary-heap/binary-heap.spec.js
+++ src/binary-heap/binary-heap.spec.js
@@ -1,66 +1,218 @@
 const { MaxBinaryHeap } = require("./binary-heap");
+const { MinBinaryHeap } = require("./binary-heap");
 
-describe("add", () => {
-  let heap;
+describe("Max binary heap", () => {
+  describe("add", () => {
+    let heap;
 
-  beforeEach(() => {
-    heap = new MaxBinaryHeap();
+    beforeEach(() => {
+      heap = new MaxBinaryHeap();
+    });
+
+    it("1 element", () => {
+      heap.add(1);
+      expect(heap.items).toEqual([1]);
+    });
+
+    it("consecutive increasing elements", () => {
+      heap.add(1);
+      heap.add(2);
+      heap.add(3);
+      expect(heap.items).toEqual([3, 1, 2]);
+    });
+
+    it("consecutive decreasing elements", () => {
+      heap.add(3);
+      heap.add(2);
+      heap.add(1);
+      expect(heap.items).toEqual([3, 2, 1]);
+    });
   });
 
-  it("1 element", () => {
-    heap.add(1);
-    expect(heap.items).toEqual([1]);
+  describe("build", () => {
+    let heap;
+
+    beforeEach(() => {
+      heap = new MaxBinaryHeap();
+    });
+
+    it("1 element", () => {
+      heap.build([1]);
+      expect(heap.items).toEqual([1]);
+    });
+
+    it("consecutive increasing elements", () => {
+      heap.build([1, 2, 3]);
+      expect(heap.items).toEqual([3, 2, 1]);
+    });
+
+    it("consecutive decreasing elements", () => {
+      heap.build([3, 2, 1]);
+      expect(heap.items).toEqual([3, 2, 1]);
+    });
   });
 
-  it("consecutive increasing elements", () => {
-    heap.add(1);
-    heap.add(2);
-    heap.add(3);
-    expect(heap.items).toEqual([3, 1, 2]);
-  });
+  describe("extract max", () => {
+    let heap;
 
-  it("consecutive decreasing elements", () => {
-    heap.add(3);
-    heap.add(2);
-    heap.add(1);
-    expect(heap.items).toEqual([3, 2, 1]);
+    beforeEach(() => {
+      heap = new MaxBinaryHeap();
+    });
+
+    it("0 element", () => {
+      expect(heap.extractMax()).toBeNull();
+    });
+
+    it("1 element", () => {
+      heap.add(1);
+      expect(heap.extractMax()).toEqual(1);
+      expect(heap.extractMax()).toBeNull();
+    });
+
+    it("consecutive increasing elements", () => {
+      heap.add(1);
+      heap.add(2);
+      heap.add(3);
+      expect(heap.extractMax()).toEqual(3);
+      expect(heap.extractMax()).toEqual(2);
+      expect(heap.extractMax()).toEqual(1);
+      expect(heap.extractMax()).toBeNull();
+    });
+
+    it("consecutive decreasing elements", () => {
+      heap.add(3);
+      heap.add(2);
+      heap.add(1);
+      expect(heap.extractMax()).toEqual(3);
+      expect(heap.extractMax()).toEqual(2);
+      expect(heap.extractMax()).toEqual(1);
+      expect(heap.extractMax()).toBeNull();
+    });
+
+    it("long consecutive increasing elements", () => {
+      const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      heap.build(input);
+      for (let index = input.length - 1; index >= 0; index--) {
+        expect(heap.extractMax()).toEqual(input[index]);
+      }
+      expect(heap.extractMax()).toBeNull();
+    });
+
+    it("long consecutive decreasing elements", () => {
+      const input = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+      heap.build(input);
+      for (let index = 0; index < input.length; index++) {
+        expect(heap.extractMax()).toEqual(input[index]);
+      }
+      expect(heap.extractMax()).toBeNull();
+    });
   });
 });
 
-describe("extract max", () => {
-  let heap;
+describe("Min binary heap", () => {
+  describe("add", () => {
+    let heap;
 
-  beforeEach(() => {
-    heap = new MaxBinaryHeap();
+    beforeEach(() => {
+      heap = new MinBinaryHeap();
+    });
+
+    it("1 element", () => {
+      heap.add(1);
+      expect(heap.items).toEqual([1]);
+    });
+
+    it("consecutive increasing elements", () => {
+      heap.add(1);
+      heap.add(2);
+      heap.add(3);
+      expect(heap.items).toEqual([1, 2, 3]);
+    });
+
+    it("consecutive decreasing elements", () => {
+      heap.add(3);
+      heap.add(2);
+      heap.add(1);
+      expect(heap.items).toEqual([1, 3, 2]);
+    });
   });
 
-  it("0 element", () => {
-    expect(heap.extractMax()).toBeNull();
+  describe("build", () => {
+    let heap;
+
+    beforeEach(() => {
+      heap = new MinBinaryHeap();
+    });
+
+    it("1 element", () => {
+      heap.build([1]);
+      expect(heap.items).toEqual([1]);
+    });
+
+    it("consecutive increasing elements", () => {
+      heap.build([1, 2, 3]);
+      expect(heap.items).toEqual([1, 2, 3]);
+    });
+
+    it("consecutive decreasing elements", () => {
+      heap.build([3, 2, 1]);
+      expect(heap.items).toEqual([1, 2, 3]);
+    });
   });
 
-  it("1 element", () => {
-    heap.add(1);
-    expect(heap.extractMax()).toEqual(1);
-    expect(heap.extractMax()).toBeNull();
-  });
+  describe("extract min", () => {
+    let heap;
 
-  it("consecutive increasing elements", () => {
-    heap.add(1);
-    heap.add(2);
-    heap.add(3);
-    expect(heap.extractMax()).toEqual(3);
-    expect(heap.extractMax()).toEqual(2);
-    expect(heap.extractMax()).toEqual(1);
-    expect(heap.extractMax()).toBeNull();
-  });
+    beforeEach(() => {
+      heap = new MinBinaryHeap();
+    });
 
-  it("consecutive decreasing elements", () => {
-    heap.add(3);
-    heap.add(2);
-    heap.add(1);
-    expect(heap.extractMax()).toEqual(3);
-    expect(heap.extractMax()).toEqual(2);
-    expect(heap.extractMax()).toEqual(1);
-    expect(heap.extractMax()).toBeNull();
+    it("0 element", () => {
+      expect(heap.extractMin()).toBeNull();
+    });
+
+    it("1 element", () => {
+      heap.add(1);
+      expect(heap.extractMin()).toEqual(1);
+      expect(heap.extractMin()).toBeNull();
+    });
+
+    it("consecutive increasing elements", () => {
+      heap.add(1);
+      heap.add(2);
+      heap.add(3);
+      expect(heap.extractMin()).toEqual(1);
+      expect(heap.extractMin()).toEqual(2);
+      expect(heap.extractMin()).toEqual(3);
+      expect(heap.extractMin()).toBeNull();
+    });
+
+    it("consecutive decreasing elements", () => {
+      heap.add(3);
+      heap.add(2);
+      heap.add(1);
+      expect(heap.extractMin()).toEqual(1);
+      expect(heap.extractMin()).toEqual(2);
+      expect(heap.extractMin()).toEqual(3);
+      expect(heap.extractMin()).toBeNull();
+    });
+
+    it("long consecutive increasing elements", () => {
+      const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      heap.build(input);
+      for (let index = 0; index < input.length; index++) {
+        expect(heap.extractMin()).toEqual(input[index]);
+      }
+      expect(heap.extractMin()).toBeNull();
+    });
+
+    it("long consecutive decreasing elements", () => {
+      const input = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+      heap.build(input);
+      for (let index = input.length - 1; index >= 0; index--) {
+        expect(heap.extractMin()).toEqual(input[index]);
+      }
+      expect(heap.extractMin()).toBeNull();
+    });
   });
 });
