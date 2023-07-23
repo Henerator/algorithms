f64a99ad1037f1ac4c36f3097e15f99209cb17a2
roman makarov <whiteblacks@yandex.ru>
2023-07-23 22:44:59 +0400

    leetcode 1387-sort-integers-v3

--- src/leetcode/1387-sort-integers/1387-sort-integers.js
+++ src/leetcode/1387-sort-integers/1387-sort-integers.js
@@ -5,15 +5,10 @@
  * @return {number}
  */
 function getKth(start, end, k) {
-  const cacheValue = new Map();
   const cachePower = new Map();
 
   function getNextValue(value) {
-    if (cacheValue.has(value)) return cacheValue.get(value);
-
-    const nextValue = value % 2 === 0 ? value >> 1 : 3 * value + 1;
-    cacheValue.set(value, nextValue);
-    return nextValue;
+    return value % 2 === 0 ? value >> 1 : 3 * value + 1;
   }
 
   function getPower(value) {
@@ -23,12 +18,12 @@ function getKth(start, end, k) {
     let nextValue = value;
 
     while (nextValue !== 1) {
-      power++;
-      nextValue = getNextValue(nextValue);
-
       if (cachePower.has(nextValue)) {
         return power + cachePower.get(nextValue);
       }
+
+      power++;
+      nextValue = getNextValue(nextValue);
     }
 
     cachePower.set(value, power);
@@ -39,8 +34,6 @@ function getKth(start, end, k) {
   for (let index = start; index <= end; index++) {
     arr.push({ value: index, power: getPower(index) });
   }
-
-  console.log(arr);
 
   return arr.sort((itemA, itemB) => {
     return itemA.power === itemB.power ? itemA.index - itemB.index : itemA.power - itemB.power;
