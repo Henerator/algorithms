21ca272d457a25e96a15ce49256d936d849c0771
roman_makarov <whiteblacks@yandex.ru>
2025-01-13 16:30:45 +0400

    leetcode 207-course-schedule-v2

--- src/leetcode/207-course-schedule/207-course-schedule.spec.js
+++ src/leetcode/207-course-schedule/207-course-schedule.spec.js
@@ -1,19 +1,42 @@
 const { canFinish } = require('./207-course-schedule');
+const { canFinish: canFinishV2 } = require('./207-course-schedule-2');
 
-it('test 1', () => {
-  const output = canFinish(2, [[1, 0]]);
-  expect(output).toEqual(true);
+describe('canFinish v1', () => {
+  it('test 1', () => {
+    const output = canFinish(2, [[1, 0]]);
+    expect(output).toEqual(true);
+  });
+
+  it('test 2', () => {
+    const output = canFinish(2, [
+      [1, 0],
+      [0, 1],
+    ]);
+    expect(output).toEqual(false);
+  });
+
+  it('test 3', () => {
+    const output = canFinish(5, []);
+    expect(output).toEqual(true);
+  });
 });
 
-it('test 2', () => {
-  const output = canFinish(2, [
-    [1, 0],
-    [0, 1],
-  ]);
-  expect(output).toEqual(false);
-});
+describe('canFinish v2', () => {
+  it('test 1', () => {
+    const output = canFinishV2(2, [[1, 0]]);
+    expect(output).toEqual(true);
+  });
 
-it('test 3', () => {
-  const output = canFinish(5, []);
-  expect(output).toEqual(true);
+  it('test 2', () => {
+    const output = canFinishV2(2, [
+      [1, 0],
+      [0, 1],
+    ]);
+    expect(output).toEqual(false);
+  });
+
+  it('test 3', () => {
+    const output = canFinishV2(5, []);
+    expect(output).toEqual(true);
+  });
 });
